ca3ac8c8ed19330084e0125b430c46185e17a5ae
timcera <>
2011-03-05 04:38:34 +0000

    * Added command line arguement 'y' to say yes to warning - for running in batch files. * Added file glob excludes in pyslice.ini - unusual case, but for situations when you don't want to delete the files from the template and they are recreated during the model run. * If file already exists in the model runs directory, for example from an earlier run of pyslice.py then pyslice.py will only copy if filecmp shows differences.

--- pyslice/pyslice.py
+++ pyslice/pyslice.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/sjr/beodata/local/python_linux/bin/python
 # pyslice.py 
 #    Copyright (C) 2001  Tim Cera timcera@earthlink.net
 #    http://home.earthlink.net/~timcera
@@ -50,6 +50,7 @@ import os.path
 import re
 import ConfigParser
 import shutil
+import filecmp
 import pyslice_lib.PySPG as pyspg
 try:
     import threading as _threading
@@ -142,7 +143,7 @@ class ConfigFileNotFoundError(Exception):
 
 class RequiredSectionNotFoundError(Exception):
     pass
-        
+
 class NotValidTypeError(Exception):
     pass
 
@@ -160,18 +161,17 @@ class Pyslice:
     #--------------------------
     def read_config(self, min_sections, max_sections, req_sections_list):
         """ Reads the pyslice.ini file.
-  
+
         """
-  
+
         # Can we find pyslice.ini?
         pyslice_ini = os.path.join(os.getcwd(), input_file)
         if not os.access(pyslice_ini, os.F_OK | os.R_OK):
             raise ConfigFileNotFoundError("%s was not found or not readable ***" % (input_file,))
-  
         # Read it in.
         config_dict = ConfigParser.ConfigParser()
         config_dict.read(pyslice_ini)
-  
+
         # Is pyslice.ini minimally error free?
         num_sections = len(config_dict.sections())
         if num_sections < min_sections or num_sections > max_sections:
@@ -179,7 +179,7 @@ class Pyslice:
         for sec in req_sections_list:
             if not config_dict.has_section(sec):
                 raise RequiredSectionNotFoundError("pyslice.ini requires the [%s] section." % sec)
-    
+
         # Return pyslice.ini as dictionary.
         return config_dict
 
@@ -187,79 +187,87 @@ class Pyslice:
     def dequote(self, in_str):
         """ Removes quotes around strings in the configuration file.
             This corrects a mistake that I would commonly make.
-  
+
         """
         in_str = string.replace(in_str, '\'', '')
         in_str = string.replace(in_str, '\"', '')
         return in_str
-  
+
 
     def path_correction(self, path):
         """ Corrects path separators and removes trailing path separators.
-  
+
         Needed so that path separators of any OS are corrected to the
         platform running the script.
-  
+
         """
         path = string.replace(path, "/", os.sep)
         path = string.replace(path, "\\", os.sep)
         if path[-1] == "/" or path[-1] == "\\":
-            path = path[:-1] 
+            path = path[:-1]
         return path
 
-    def create_output(self, var_set, dirname, fnames):
+    def create_output(self, var_set, dirname, dirs, fnames):
         try:
             os.makedirs(os.path.join(_output_path, _strtag))
         except OSError:
             pass
+        for dirn in dirs:
+            infilepath = os.path.join(dirname, dirn)
+            rel_dir = infilepath.replace(_template_path + os.sep, '')
+            try:
+                os.makedirs(os.path.join(_output_path, _strtag, rel_dir))
+            except OSError:
+                pass
         for files in fnames:
+            excludeflag = False
+            for extensions in _exclude_list:
+                if extensions in files:
+                    excludeflag = True
+            if excludeflag:
+                continue
             infilepath = os.path.join(dirname, files)
             rel_dir = infilepath.replace(_template_path + os.sep, '')
             outfilepath = os.path.join(_output_path, _strtag, rel_dir)
-  
-            # Sub-directories in template directory
-            if os.path.isdir(infilepath):
-                try:
-                    os.makedirs(outfilepath)
-                    continue
-                except OSError:
-                    continue
-  
+
             # Is this a text file?  If so, just open as a template
             if istext(infilepath):
                 inputf = open(infilepath, 'r')
             else:
-                shutil.copy(infilepath, outfilepath)
+                try:
+                    equalfiles = filecmp.cmp(infilepath, outfilepath)
+                except OSError:
+                    equalfiles = False
+                if not equalfiles:
+                    shutil.copy(infilepath, outfilepath)
                 continue
-  
-            output = open(outfilepath,'w')
+
+            output = open(outfilepath, 'w')
             shutil.copystat(infilepath, outfilepath)
-  
+
             # Search for _keywordvarname_keyword and replace with appropriate
             # value.
+            escaped_keyword = re.escape(_keyword)
+            keyword_search = re.compile(escaped_keyword)
+            search_for = re.compile(
+                escaped_keyword + '(.*?)' + escaped_keyword)
             for line in inputf:
-                for variables in var_set[1:]:
-                    var_name = variables[0]
-                    var_value = variables[1]
-                    # compile re search for _keyword .* variable_name .*
-                    # _keyword
-                    search_for = re.compile(
-                        re.escape(_keyword) + '(' + r'.*?' + 
-                        var_name + r'.*?' + ')' + re.escape(_keyword))
-                    # while there are still matchs available on the line
-                    while search_for.search(line):
-                        # only have 1 group, but it returns a 2 item tuple,
-                        # need [0]
-                        match = search_for.search(line).groups()[0]
-                        # replace variable name with number
-                        match = string.replace(match, var_name, str(var_value))
-                        # evaluate Python statement with eval
-                        match = eval(match)
-                        # replace variable with calculated value
-                        line = re.sub(search_for, str(match), line, count=1)
+                if keyword_search.search(line):
+                    for matches in search_for.findall(line):
+                        for variables in var_set[1:]:
+                            var_name = variables[0]
+                            var_value = variables[1]
+                            if var_name in matches:
+                                exec('%s = %s' % (var_name, var_value))
+                                # replace variable name with number
+                                match = string.replace(matches, var_name, str(var_value))
+                                # evaluate Python statement with eval
+                                match = eval(match)
+                                # replace variable with calculated value
+                                line = re.sub(escaped_keyword + matches + escaped_keyword, str(match), line, count=1)
                 # writes new line out to output file
                 output.write(line)
-  
+
             inputf.close()
             output.close()
 
@@ -273,7 +281,7 @@ class Pyslice:
             # close_fds is not supported on Windows
             p = subprocess.Popen(com, shell=True, stdin=subprocess.PIPE,
                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        
+
         (chstdin, chstdouterr) = (p.stdin, p.stdout)
 
         fo = open('pyslice.log', 'w')
@@ -285,19 +293,21 @@ class Pyslice:
         global _output_path
         global _keyword
         global _template_path
+        global _exclude_list
 
         ftn = "Pyslice.run"
         debug(ftn,"hello, world")
 
-        raw_input("""
-        Pyslice will replace all files in the output directories with the same
-        name as files in the template directory.  If this is not what you want,
-        move the files as necessary.  Pyslice also does not check if the
-        permutation schedule is the same and may not use the same number of
-        output directories from previous pyslice runs. 
+        if len(sys.argv) == 1:
+            raw_input("""
+            Pyslice will replace all files in the output directories with the same
+            name as files in the template directory.  If this is not what you want,
+            move the files as necessary.  Pyslice also does not check if the
+            permutation schedule is the same and may not use the same number of
+            output directories from previous pyslice runs. 
 
-        'Press any key to continue . . .'
-                  """)
+            'Press any key to continue . . .'
+                      """)
 
         # Read the configuration file and set appropriate variables.
         configuration = self.read_config(4, 100, ["paths", "flags", "program"])
@@ -306,6 +316,8 @@ class Pyslice:
         _output_path = self.dequote(configuration.get("paths", "output_path"))
         _keyword = self.dequote(configuration.get("flags", "keyword"))
         max_threads = configuration.getint("flags", "max_threads")
+        if configuration.has_option('flags', 'exclude_copy'):
+            _exclude_list = eval(configuration.get('flags', 'exclude_copy'))
         if max_threads <= 0:
             max_threads = total_processes
         flat_dirs = configuration.getboolean("flags", "flat_dirs")
@@ -386,8 +398,13 @@ class Pyslice:
             set.append(tmp)
 
         while 1:
+            try:
+                if sys.argv[1] == 'y':
+                    break
+            except IndexError:
+                pass
             inp =  raw_input(
-                '''Configuration results in %s permutations. Continue? (y/n) > ''' 
+                '''Configuration results in %s permutations. Continue? (y/n) > '''
                 % (len(set),)
                 )
             if not inp:
@@ -408,7 +425,9 @@ class Pyslice:
 
             # Create the files and directories from the template
             # Walks the _template_path directory structure
-            os.path.walk(_template_path, self.create_output, var_set)
+            #os.path.walk(_template_path, self.create_output, var_set)
+            for root, dirs, files in os.walk(_template_path):
+                self.create_output(var_set, root, dirs, files)
 
 
             # Wait until there are less than max_threads.
@@ -428,7 +447,7 @@ class Pyslice:
 def main(option_dict):
     main_x = Pyslice()
     main_x.run()
-      
+
 #-------------------------
 if __name__ == '__main__':
     ftn = "main"
