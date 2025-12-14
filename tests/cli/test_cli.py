32c786b4d4f71538ecb7282c68227f644b0ba7f6
Tim Cera <tim@cerazone.net>
2025-12-14 18:59:39 -0500

    ci: fix test on Windows and maybe Mac

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -12,6 +12,6 @@ def test_cli(tmp_path):
     shutil.copytree(".", newdir, dirs_exist_ok=True)
     os.chdir(newdir)
     subprocess.run(["pyslice"], input="\ny\n", text=True, check=True)
-    dirs = [str(i) for i in sorted(list(Path("output").rglob("*")))]
+    dirs = [str(i.as_posix()) for i in sorted(list(Path("output").rglob("*")))]
     refdirs = json.load(open("refdirs.json"))
     assert dirs == refdirs
