51e572bf8bf4b475035e468e371ec9f67d47deb5
Tim Cera <tim@cerazone.net>
2025-12-14 20:15:02 -0500

    test: additional work to make tests work across platforms

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -13,7 +13,7 @@ def test_cli(tmp_path):
     shutil.copytree(".", newdir, dirs_exist_ok=True)
     os.chdir(newdir)
     subprocess.run(["pyslice"], input="\ny\n", text=True, check=True)
-    time.sleep(10)
+    time.sleep(20)
     dirs = [str(i.as_posix()) for i in sorted(list(Path("output").rglob("*")))]
     refdirs = json.load(open("refdirs.json"))
     assert dirs == refdirs
